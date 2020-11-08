03bdb25a6195f29a6d21bff6528b3d992cadb7f2
vaibhavp <vp15591@mail.com>
2020-11-08 13:54:28 +0530

    added foregroud script

--- k8s-cluster/background.sh
+++ k8s-cluster/background.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no node01 ./break-cluster.sh
-while true
+ssh -o StrictHostKeyChecking=no node01 'service kubelet stop'
 do
     var=`kubectl get nodes node01 -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}'`
     if [[ $var != "True" ]];then
