e3fdc1777237f9bb2162d4d4ee4a3710cde7b6c1
vaibhavp <vp15591@mail.com>
2020-11-08 14:13:30 +0530

    added foregroud script

--- k8s-cluster/foreground.sh
+++ k8s-cluster/foreground.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 echo "Waiting to complete"; 
+while true
 do
     var=`kubectl get nodes node01 -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}'`
     if [[ $var != "True" ]];then
